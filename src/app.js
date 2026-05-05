6dcfa605c826b48e34abe2a7f8d2316876003bbd
humaira8429 <humaira.hashmi@hcltech.com>
2026-05-05 13:22:13 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('CI/CD PoC Application is running successfully on Feature branch');
+  res.send('CI/CD PoC Application is running successfully on Feature branches');
 });
 
 // Start server ONLY when running app directly
