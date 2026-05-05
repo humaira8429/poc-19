d5624adc394ba117a298551ff390bcf497c1558f
humaira8429 <humaira.hashmi@hcltech.com>
2026-05-05 13:07:52 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -22,29 +22,4 @@ if (require.main === module) {
 
 }
  
-module.exports = app;const express = require('express');
-
-const app = express();
- 
-app.get('/', (req, res) => {
-
-  res.send('CI/CD PoC Application is running successfully on Feature branch');
-
-});
- 
-// Start server ONLY when running app directly
-
-if (require.main === module) {
-
-  const port = process.env.PORT || 3000;
-
-  app.listen(port, () => {
-
-    console.log(`App running on port ${port}`);
-
-  });
-
-}
- 
 module.exports = app;
- 
